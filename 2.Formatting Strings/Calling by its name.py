7baf6dee26096e67a2f2326a61e82c76d438ad13
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-24 16:48:14 +0700

    Create Calling by its name.py

---
+++ 2.Formatting Strings/Calling by its name.py
@@ -0,0 +1,11 @@
+# Create a dictionary
+plan = {
+  		"field": courses[0],
+        "tool": courses[1]
+        }
+
+# Complete the placeholders accessing elements of field and tool keys in the data dictionary
+my_message = "If you are interested in {data[field]}, you can take the course related to {data[tool]}"
+
+# Use the plan dictionary to replace placeholders
+print(my_message.format(data=plan))
