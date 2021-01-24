705088764bf48ac4e6c9f1f566b741be811360fb
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-24 16:42:54 +0700

    Create Artificial reviews.py

---
+++ 1.Basic Concepts of String Manipulation/Artificial reviews.py
@@ -0,0 +1,8 @@
+# Select the first 32 characters of movie1
+first_part = movie1[:32]
+
+# Select from 43rd character to the end of movie1
+last_part = movie1[42:]
+
+# Select from 33rd to the 42nd character
+middle_part = movie2[33-1:42]
