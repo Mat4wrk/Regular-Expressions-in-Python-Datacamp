2baeb99432275375ef3af1d38fdc3c2fa8fa868f
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-24 16:43:29 +0700

    Create Palindromes.py

---
+++ 1.Basic Concepts of String Manipulation/Palindromes.py
@@ -0,0 +1,9 @@
+# Get the word
+movie_title = movie[12-1:30]
+
+# Obtain the palindrome
+palindrome = movie[11:30]
+
+# Print the word if it's a palindrome
+if movie_title == palindrome:
+	print(movie_title)
